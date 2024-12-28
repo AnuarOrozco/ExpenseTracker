a5305c14ea536012a5522c8dfeac336bcfacf9a6
Anuar Orozco <anuarorozzco@gmail.com>
2024-12-27 22:53:08 -0600

    controller-package: improved the method showDashboard and did some changes to the html improves thymeleaf annotations

--- src/main/java/com/aocotechsolutions/expense_tracker/controller/ExpenseController.java
+++ src/main/java/com/aocotechsolutions/expense_tracker/controller/ExpenseController.java
@@ -1,9 +1,14 @@
 package com.aocotechsolutions.expense_tracker.controller;
 
+import com.aocotechsolutions.expense_tracker.model.Expense;
 import com.aocotechsolutions.expense_tracker.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Controller
 public class ExpenseController {
@@ -12,7 +17,12 @@ public class ExpenseController {
     private ExpenseService expenseService;
 
     @GetMapping("/")
-    public String showDashboard() {
+    public String showDashboard(Model model) {
+        List<Expense> expenses = expenseService.getAllExpenses();
+        BigDecimal totalAmount = expenses.stream().map(Expense::getAmount).reduce(BigDecimal.ZERO, BigDecimal::add);
+        model.addAttribute("expenses", expenses);
+        model.addAttribute("totalAmount", totalAmount);
+
         return "Dashboard";
     }
 
