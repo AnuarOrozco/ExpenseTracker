9c142a0899c6d4913717b525d05846f15f063f8f
Anuar Orozco <anuarorozzco@gmail.com>
2024-12-27 23:36:22 -0600

    Final touch

--- src/main/java/com/aocotechsolutions/expense_tracker/model/Expense.java
+++ src/main/java/com/aocotechsolutions/expense_tracker/model/Expense.java
@@ -21,4 +21,27 @@ public class Expense {
     private String description;
     private BigDecimal amount;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 }
